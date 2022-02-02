43b65ce62623d01bb1b19093a306bcab55d6c3d6
Marcelo V Goncalves <cellobnu@gmail.com>
2022-02-02 00:53:11 -0300

    created display data in to screen

--- src/components/Weather/components/Details/index.js
+++ src/components/Weather/components/Details/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import WeatherContainer from '../../containers/WeatherContainer'
 import useLocation from './../../../../services/location';
 
-import {ContentView, Text, Button} from './styles';
+import {ContentView, CountWeather, TempText, NameCountryText, MaxMinText, Text} from './styles';
 
 const WeatherScreen = () => {
   const locationCurrent = useLocation()
@@ -10,16 +10,21 @@ const WeatherScreen = () => {
   return (
     <WeatherContainer locationCurrent={locationCurrent}>
       {({weatherUser}) => {
-        console.log('weatherUser', weatherUser);
         if(weatherUser?.data){
           const {max, min, temp, name, country, weather} = weatherUser?.data || ''
-          //const weatherDescription = weather[0]?.description || ''
+          console.log('weather', weather);
+          const weatherDescription = weather[0].description
           return (
             <ContentView>
-              <Text>{name}-{country}</Text>
-              <Text>{temp}</Text>
-              <Text>{max}-{min}</Text>
-              {/* <Text>{weatherDescription}</Text> */}
+              
+              <CountWeather>
+                <NameCountryText>{name}-{country}</NameCountryText>
+                <TempText>{temp} Cº</TempText>
+                <MaxMinText>Máx.:{max} - Min.:{min}</MaxMinText>
+                <Text>{weatherDescription}</Text>
+              </CountWeather>
+              
+              
               
             </ContentView>
           )
--- src/components/Weather/components/Details/styles.js
+++ src/components/Weather/components/Details/styles.js
@@ -7,10 +7,27 @@ export const ContentView = styled.View`
   justify-content: center;
 `;
 
+export const CountWeather = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+`
 export const Text = styled.Text`
   font-size: 20px;
-  ${({colorText}) =>
-    css`
-      color: ${colorText || '#000'};
-    `}
+  color: #000;
+`;
+
+export const NameCountryText = styled.Text`
+  font-size: 20px;
+  color: #000;
+`;
+
+export const TempText = styled.Text`
+  font-size: 40px;
+  color: #000;
+`;
+
+export const MaxMinText = styled.Text`
+  font-size: 12px;
+  color: #000;
 `;
